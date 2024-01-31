04684f2f047b8cbbe65bc3f986a95ba4dfc5de3f
shegsharem <logan72091@gmail.com>
2024-01-31 12:20:29 -0600

    hello

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("Window")
-        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.resize(400, 200)
+        #self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground,True)
+        #self.setFixedSize(400,300)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
 
-        button = QPushButton("Press Here")
-        self.setFixedSize(400,300)
 
-        self.setCentralWidget(button)
+if __name__ == "__main__":
+    app = QApplication([])
+    app.setStyle('windows')
 
-app = QApplication([])
-app.setStyle('windows')
-
-window = MainWindow()
-window.show()
-
-app.exec() # main loop
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec()) # main loop
